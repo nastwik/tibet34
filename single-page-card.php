70427b5060c22d0637414d3dca5bf730b37430ee
nastwik <anastasrassoh@gmail.com>
2024-07-08 10:45:34 +0300

    table in trade-concrete page

---
+++ single-page-card.php
@@ -0,0 +1,37 @@
+<? require_once 'header.php' ?>
+<header class="page-header">
+  <div class="container">
+    <h2>Товарный бетон</h2>
+	  <ul>
+	  	<li><a href="#">Главная</a></li>
+		<li>Каталог</li>
+	  	<li>Товарный бетон</li>
+	  </ul>
+  </div> 
+</header>
+	<section class="content-section ">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3">
+					<aside class="sidebar">
+						<div class="widget">
+							<h6 class="widget-title">КАТЕГОРИИ</h6>
+							<ul class="categories">
+								<li><a href="#">Бетон с доставкой</a></li>
+									<ul class="subcategories">
+										<li><a href="#">Товарный бетон</a></li>
+										<li><a href="#">Усиленный бетон</a></li>
+									</ul>
+								<li><a href="#">Тротуарная плитка</a></li>
+							</ul>
+						</div>
+					</aside>
+				</div>
+                <div class="col-lg-9">
+                </div>
+                </div>
+            </div>
+    </section>
+        
+
+<? require_once 'footer.php' ?>	
--- trade-concrete.php
+++ trade-concrete.php
@@ -11,16 +11,9 @@
 </header>
 	<section class="content-section ">
 		<div class="container">
-			<div class="row no-gutters">
+			<div class="row">
 				<div class="col-lg-3">
 					<aside class="sidebar">
-						<!-- <div class="widget">
-							<h6 class="widget-title">SEARCH</h6>
-							<form>
-								<input type="search" placeholder="Type here to search">
-								<input type="submit" value="Search">
-							</form>
-						</div> -->
 						<div class="widget">
 							<h6 class="widget-title">КАТЕГОРИИ</h6>
 							<ul class="categories">
@@ -32,129 +25,211 @@
 								<li><a href="#">Тротуарная плитка</a></li>
 							</ul>
 						</div>
-					
-						<div class="widget">
-							<h6 class="widget-title">GALLERY</h6>
-							<figure>
-								<img src="images/slide01.jpg" alt="Image">
-							</figure>				
-							<figure>
-								<img src="images/slide02.jpg" alt="Image">
-							</figure>
-						</div>
 					</aside>
 				</div>
-				<!-- <div class="col-lg-3"> 
-					<a href="#" class="card-product"> 
-						<div class="card-product__content">
-							<img src="images/mixed_concrete.jpg">
-							<div class="card-product__name"><span>Товарный бетон</span></div> 
-						</div>
-					</a> 
-				</div>
-				<div class="col-lg-3"> 
-					<a href="#" class="card-product"> 
-						<div class="card-product__content">
-							<img src="images/c_b_c.jpg">
-							<div class="card-product__name"><span>Усиленный бетон</span></div> 
-						</div>
-					</a> 
-				</div>
-            <div class="col-lg-3"> 
-					<a href="#" class="card-product"> 
-						<div class="card-product__content">
-							<img src="images/c_b_c.jpg">
-							<div class="card-product__name"><span>Усиленный бетон</span></div> 
-						</div>
-					</a> 
-				</div> -->
-            <div class="col-lg-8">
-               <div class="product__container">
-                <div class="product">
-                    <div class="product-img">
-                        <a href="#"><img src="images/concrete01.jpg" alt="concrete"></a>
+                <div class="col-lg-9">
+                    <div class="product__container">
+                        <div class="product">
+                            <div class="product-img">
+                                <img src="images/concrete01.jpg" alt="concrete">
+                            </div>
+                            <p class="product-title">
+                                <a href="#">Бетон М-400П3 (В 30)</a>
+                            </p>
+                            <div class="product-desc">
+                                <p>В наличии</p>
+                            </div>
+                            <!-- <div class="service__price">7000 руб/м3 руб.</div> -->
+                            <p class="product-price">7000 руб/м3</p>
+                        </div>
+                        <div class="product">
+                            <div class="product-img">
+                                <a href="#"><img src="images/concrete02.jpg" alt="concrete"></a>
+                            </div>
+                            <p class="product-title">
+                                <a href="#">Бетон М-350П4 (В 25)</a>
+                            </p>
+                            <div class="product-desc">
+                                <p>В наличии</p>
+                            </div>
+                            <p class="product-price">6900 руб/м3</p>
+                        </div>
+                        <div class="product">
+                            <div class="product-img">
+                                <a href="#"><img src="images/concrete03.jpg" alt="concrete"></a>
+                            </div>
+                            <p class="product-title">
+                                <a href="#">Бетон М-400П2 (В 30)</a>
+                            </p>
+                            <div class="product-desc">
+                                <p>В наличии</p>
+                                <p>Подходит для бетононасоса</p>
+                            </div>
+                            <p class="product-price">6900 руб/м3</p>
+                        </div>
+                        </div>
+                        <div class="product__container">
+                        <div class="product">
+                            <div class="product-img">
+                                <a href="#"><img src="images/concrete03.jpg" alt="concrete"></a>
+                            </div>
+                            <p class="product-title">
+                                <a href="#">Бетон М-400П3 (В 30)</a>
+                            </p>
+                            <div class="product-desc">
+                                <p>В наличии</p>
+                                <p>Подходит для бетононасоса</p>
+                            </div>
+                            <p class="product-price">7000 руб/м3</p>
+                        </div>
+                        <div class="product">
+                            <div class="product-img">
+                                <a href="#"><img src="images/concrete01.jpg" alt="concrete"></a>
+                            </div>
+                            <p class="product-title">
+                                <a href="#">Бетон М-350П4 (В 25)</a>
+                            </p>
+                            <div class="product-desc">
+                                <p>В наличии</p>
+                                <p>Подходит для бетононасоса</p>
+                            </div>
+                            <p class="product-price">6900 руб/м3</p>
+                        </div>
+                        <div class="product">
+                            <div class="product-img">
+                                <a href="#"><img src="images/concrete02.jpg" alt="concrete"></a>
+                            </div>
+                            <p class="product-title">
+                                <a href="#">Бетон М-400П2 (В 30)</a>
+                            </p>
+                            <div class="product-desc">
+                                <p>В наличии</p>
+                            </div>
+                            <p class="product-price">6900 руб/м3</p>
+                        </div>
                     </div>
-                    <p class="product-title">
-                        <a href="#">Бетон М-400П3 (В 30)</a>
-                    </p>
-                    <div class="product-desc">
-                        <p>В наличии</p>
-                    </div>
-                    <p class="product-price">7000 руб/м3</p>
                 </div>
-                <div class="product">
-                    <div class="product-img">
-                        <a href="#"><img src="images/concrete02.jpg" alt="concrete"></a>
-                    </div>
-                    <p class="product-title">
-                        <a href="#">Бетон М-350П4 (В 25)</a>
-                    </p>
-                    <div class="product-desc">
-                        <p>В наличии</p>
-                    </div>
-                    <p class="product-price">6900 руб/м3</p>
-                </div>
-                <div class="product">
-                    <div class="product-img">
-                        <a href="#"><img src="images/concrete03.jpg" alt="concrete"></a>
-                    </div>
-                    <p class="product-title">
-                        <a href="#">Бетон М-400П2 (В 30)</a>
-                    </p>
-                    <div class="product-desc">
-                        <p>В наличии</p>
-                        <p>Подходит для бетононасоса</p>
-                    </div>
-                    <p class="product-price">6900 руб/м3</p>
-                </div>
-                </div>
-                <div class="product__container">
-                <div class="product">
-                    <div class="product-img">
-                        <a href="#"><img src="images/concrete03.jpg" alt="concrete"></a>
-                    </div>
-                    <p class="product-title">
-                        <a href="#">Бетон М-400П3 (В 30)</a>
-                    </p>
-                    <div class="product-desc">
-                        <p>В наличии</p>
-                        <p>Подходит для бетононасоса</p>
-                    </div>
-                    <p class="product-price">7000 руб/м3</p>
-                </div>
-                <div class="product">
-                    <div class="product-img">
-                        <a href="#"><img src="images/concrete01.jpg" alt="concrete"></a>
-                    </div>
-                    <p class="product-title">
-                        <a href="#">Бетон М-350П4 (В 25)</a>
-                    </p>
-                    <div class="product-desc">
-                        <p>В наличии</p>
-                        <p>Подходит для бетононасоса</p>
-                    </div>
-                    <p class="product-price">6900 руб/м3</p>
-                </div>
-                <div class="product">
-                    <div class="product-img">
-                        <a href="#"><img src="images/concrete02.jpg" alt="concrete"></a>
-                    </div>
-                    <p class="product-title">
-                        <a href="#">Бетон М-400П2 (В 30)</a>
-                    </p>
-                    <div class="product-desc">
-                        <p>В наличии</p>
-                    </div>
-                    <p class="product-price">6900 руб/м3</p>
-                </div>
+                <section class="pricing-section centred">
+                        <div class="col-12">
+                            <div class="section-title">
+                                <h6>Предоставляем</h6>
+                                <h2>только выгодные цены</h2>
+                            </div>
+                        </div>
+                        <div class="concrete-price">
+                            <div class="fl-table">
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>Наименование конструкции</th>
+                                            <th>Оптимальная марка</th>
+                                            <th>Предпочтительная марка*</th>
+                                            <th>Стоимость, руб. **</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Ленточный фундамент</td>
+                                            <td>М250 (В 20)</td>
+                                            <td>М-300 (В 22,5)</td>
+                                            <td>от 5900</td>
+                                        </tr>
+                                        <tr class="gray">
+                                            <td>Фундаментная плита</td>
+                                            <td>М300 (В 22,5)</td>
+                                            <td>М350 (В 25)</td>
+                                            <td>от 6300</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Элементы забора</td>
+                                            <td>М200 (В 15)</td>
+                                            <td>М300 (В 22,5)</td>
+                                            <td>от 5700</td>
+                                        </tr>
+                                        <tr class="gray">
+                                            <td>Бассейн</td>
+                                            <td>М350 (В 25)</td>
+                                            <td>М400 (В 30)</td>
+                                            <td>от 6500</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Дорожки</td>
+                                            <td>М150 (В 12,5)</td>
+                                            <td>М200 (В 15)</td>
+                                            <td>от 5500</td>
+                                        </tr>
+                                        <tr class="gray">
+                                            <td>Колонны</td>
+                                            <td>М350 (В 25)</td>
+                                            <td>М350 (В 25)</td>
+                                            <td>от 6500</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Плиты перекрытия</td>
+                                            <td>М350 (В 25)</td>
+                                            <td>М400 (В 30)</td>
+                                            <td>от 6500</td>
+                                        </tr>
+                                        <tr class="gray">
+                                            <td rowspan="2">Стяжка пола</td>
+                                            <td>Раствор М150-200</td>
+                                            <td>Раствор М150-200 </td>
+                                            <td>от 4450</td>
+                                        </tr>
+                                        <tr class="gray">
+                                            <td>Бетон М100(В 7,5)</td>
+                                            <td>Бетон М150 (В 12,5)</td>
+                                            <td>от 5300</td>
+                                        </tr>
+                                        <tr>
+                                            <td rowspan="2">Отмостка</td>
+                                            <td>Раствор М150-200</td>
+                                            <td>Раствор М150-200</td>
+                                            <td>от 4450</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Бетон М150 (В 12,5)</td>
+                                            <td>Бетон М350 (В 25)</td>
+                                            <td>от 5500</td>
+                                        </tr>
+                                        <tr class="gray">
+                                            <td>Элементы колодца</td>
+                                            <td>М150 (В 12,5)</td>
+                                            <td>М200 (В 15)</td>
+                                            <td>от 5500</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Лестницы</td>
+                                            <td>М250 (В 20)</td>
+                                            <td>М300 (В 22,5)</td>
+                                            <td>от 5900</td>
+                                        </tr>
+                                        <tr class="gray">
+                                            <td>Стены</td>
+                                            <td>М300 (В 22,5)</td>
+                                            <td>М350 (В 25)</td>
+                                            <td>от 6300</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Фундамент под баню</td>
+                                            <td>М200 (В 15)</td>
+                                            <td>М250 (В 20) </td>
+                                            <td>от 5700</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <p>
+                                Предпочтительная марка рекомендуется исходя из следующих условий долговечности:<br>
+                                1.Частое изменение климатических условий
+                                (перепады температуры)<br>
+                                2. Выполнение бетонных работ рабочими с малым опытом<br>
+                                3. Человеческий фактор — некачественное выполнение работ
+                            </p>
+                        </div>
+                    </section>
             </div>
-         </div>
-   
-            далее таблица с concrete.tibet
-
-			</div>
-		</div>
-	</section>
-	
+    </section>
+        
 
 <? require_once 'footer.php' ?>	
